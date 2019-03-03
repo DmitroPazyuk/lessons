bfbe0002cd6abccc21abae74529e3c71e39a6f20
DmitroPazyuk <dmitro.pazyuk@gmail.com>
2019-03-04 01:09:34 +0300

    #2done

---
+++ lesson1/task2.js
@@ -0,0 +1,14 @@
+for(var i = 1; i<=100; i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+        continue;
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+        continue;
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+        continue;
+    } {
+        console.log(i);
+    }
+}
